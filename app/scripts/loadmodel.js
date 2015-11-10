f4143ebc00db50cf82cc6640b66f68eaa049a9d9
Fedor Baart <fedor.baart@deltares.nl>
2015-11-10 18:16:59 +0100

    pink grid

--- app/scripts/loadmodel.js
+++ app/scripts/loadmodel.js
@@ -1,8 +1,48 @@
-fetch('models/grid.json')
-	.then(function(response) {
-		return response.json();
-	})
-	.then(function(json){
-		console.log(json);
-	});
+function renderModel(json) {
+  console.log(map);
+  var svg = d3.select(map.getPanes().overlayPane).append('svg');
+  var g = svg.append('g').attr('class', 'leaflet-zoom-hide');
 
+  function projectPoint(x, y) {
+    var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+    this.stream.point(point.x, point.y);
+  }
+
+  var transform = d3.geo.transform({point: projectPoint});
+  var path = d3.geo.path().projection(transform);
+  // Reposition the SVG to cover the features.
+  function reset() {
+    var bounds = path.bounds(json),
+        topLeft = bounds[0],
+        bottomRight = bounds[1];
+    svg
+      .attr('width', bottomRight[0] - topLeft[0])
+      .attr('height', bottomRight[1] - topLeft[1])
+      .style('left', topLeft[0] + 'px')
+      .style('top', topLeft[1] + 'px');
+
+    g
+      .attr('transform', 'translate(' + -topLeft[0] + ',' + -topLeft[1] + ')');
+
+    features
+      .attr('d', path);
+
+  }
+  var features = g.selectAll('path')
+        .data(_.slice(json.features, 10000, 20000))
+        .enter()
+        .append('path')
+        .attr('d', path);
+
+
+  map.on('viewreset', reset);
+  reset();
+}
+fetch('models/grid.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json){
+    console.log(json);
+    renderModel(json);
+  });
--- app/scripts/map.js
+++ app/scripts/map.js
@@ -1,6 +1,7 @@
+var map;
 (function(){
   'use strict';
-  var map = L.map('map').setView([51.95, 4.15], 13);
+  map = L.map('map').setView([51.95, 4.15], 13);
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
